3c40e41c8deedb040d5493e73a06b5553c8805fb
kitaev <a.kitayeu@sam-solutions.net>
2022-08-25 21:58:40 +0300

    Initial commit

---
+++ src/test/java/com/samsolutions/kitayeu/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsolutions.kitayeu.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
