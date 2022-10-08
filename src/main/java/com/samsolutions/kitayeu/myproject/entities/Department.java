e5c73f1e26621437c806054696fe6d1a143b45c3
Alec Kotovich <A.Katovich@sam-solutions.com>
2022-10-08 09:06:18 +0300

    Fixed tests

--- src/main/java/com/samsolutions/kitayeu/myproject/entities/Department.java
+++ src/main/java/com/samsolutions/kitayeu/myproject/entities/Department.java
@@ -1,7 +1,10 @@
 package com.samsolutions.kitayeu.myproject.entities;
 
-import javax.persistence.*;
-import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table (name="department")
--- src/main/resources/db/migration/V1__initialdb.sql
+++ src/main/resources/db/migration/V1__initialdb.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS public.department;
 CREATE TABLE IF NOT EXISTS public.department
 (
     department_id integer NOT NULL,
-    department_name text COLLATE pg_catalog."default" NOT NULL,
+    department_name text NOT NULL,
     CONSTRAINT department_pkey PRIMARY KEY (department_id)
 );
 
@@ -13,11 +13,11 @@ DROP TABLE IF EXISTS public.employee;
 CREATE TABLE IF NOT EXISTS public.employee
 (
     employee_id integer NOT NULL,
-    firstname text COLLATE pg_catalog."default" NOT NULL,
-    lastname text COLLATE pg_catalog."default" NOT NULL,
+    firstname text  NOT NULL,
+    lastname text  NOT NULL,
     birthdate date NOT NULL,
-    gender "char" NOT NULL,
-    passport_id character(1) COLLATE pg_catalog."default" NOT NULL,
+    gender CHAR(1) NOT NULL,
+    passport_id character(1)  NOT NULL,
     passport_validity date NOT NULL,
     department_id integer NOT NULL,
     CONSTRAINT employee_pkey PRIMARY KEY (employee_id)
@@ -38,28 +38,28 @@ DROP TABLE IF EXISTS public.role;
 CREATE TABLE IF NOT EXISTS public.role
 (
     role_id integer NOT NULL,
-    role_name text COLLATE pg_catalog."default" NOT NULL,
+    role_name text  NOT NULL,
     CONSTRAINT role_pkey PRIMARY KEY (role_id)
 );
 
 
 ALTER TABLE IF EXISTS public.employee
     ADD CONSTRAINT employee FOREIGN KEY (department_id)
-        REFERENCES public.department (department_id) MATCH SIMPLE
+        REFERENCES public.department (department_id) 
         ON UPDATE NO ACTION
         ON DELETE NO ACTION;
 
 
 ALTER TABLE IF EXISTS public.employee_role
     ADD CONSTRAINT employee_id FOREIGN KEY (employee_id)
-        REFERENCES public.employee (employee_id) MATCH SIMPLE
+        REFERENCES public.employee (employee_id) 
         ON UPDATE NO ACTION
         ON DELETE NO ACTION;
 
 
 ALTER TABLE IF EXISTS public.employee_role
     ADD CONSTRAINT role_id FOREIGN KEY (role_id)
-        REFERENCES public.role (role_id) MATCH SIMPLE
+        REFERENCES public.role (role_id) 
         ON UPDATE NO ACTION
         ON DELETE NO ACTION;
 
