c02d8b8164392547bcfe3ff757e92b8c1d8ff58a
kitaev <a.kitayeu@sam-solutions.net>
2023-06-27 21:23:35 +0300

    Fix application.properties

--- src/test/java/com/samsolutions/kitayeu/myproject/controllers/mvc/HelloControllerTest.java
+++ src/test/java/com/samsolutions/kitayeu/myproject/controllers/mvc/HelloControllerTest.java
@@ -1,11 +1,5 @@
 package com.samsolutions.kitayeu.myproject.controllers.mvc;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +7,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -34,7 +34,7 @@ public class HelloControllerTest {
 								.with(csrf()))
 				.andExpect(status().isOk())
 				.andExpect(content().string(
-						equalTo("<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<meta charset=\"UTF-8\">\r\n<title>My page</title>\r\n</head>\r\n<body>\r\n\t<h1>Hello,\r\n\t\t<span>Александр!</span></h1>\r\n\t<br/>\r\n\t<br/>\r\n\t<a href=\"/departments\">List of departments</a>\r\n\t<br/>\r\n\t<br/>\r\n\t<a href=\"/roles\">List of roles</a>\r\n\t<br/>\r\n\t<br/>\r\n\t<a href=\"/employees\">List of employees</a>\r\n</body>\r\n</html>")));
+						equalTo("<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<meta charset=\"UTF-8\">\r\n<title>My page</title>\r\n</head>\r\n<body>\r\n\t<h1>Hello,\r\n\t\t<span>this is my first spring application</span></h1>\r\n\t<br/>\r\n\t<br/>\r\n\t<a href=\"/departments\">List of departments</a>\r\n\t<br/>\r\n\t<br/>\r\n\t<a href=\"/roles\">List of roles</a>\r\n\t<br/>\r\n\t<br/>\r\n\t<a href=\"/employees\">List of employees</a>\r\n\t<br/>\r\n\t<br/>\r\n\t<a href=\"/users\">List of users</a>\r\n</body>\r\n</html>")));
 	}
 
 	@Test
