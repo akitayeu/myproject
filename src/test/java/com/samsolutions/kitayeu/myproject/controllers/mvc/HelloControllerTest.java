8092abf92c7f8b4c025da81688708e8704c4e637
kitaev <a.kitayeu@sam-solutions.net>
2023-06-29 09:01:38 +0300

    Test commit for test of protection rule

--- src/test/java/com/samsolutions/kitayeu/myproject/controllers/mvc/HelloControllerTest.java
+++ src/test/java/com/samsolutions/kitayeu/myproject/controllers/mvc/HelloControllerTest.java
@@ -34,6 +34,7 @@ public class HelloControllerTest {
 						equalTo("<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<meta charset=\"UTF-8\">\r\n<title>My page</title>\r\n</head>\r\n<body>\r\n\t<h1>Hello,\r\n\t\t<span>this is my first spring application</span></h1>\r\n\t<br/>\r\n\t<br/>\r\n\t<a href=\"/departments\">List of departments</a>\r\n\t<br/>\r\n\t<br/>\r\n\t<a href=\"/roles\">List of roles</a>\r\n\t<br/>\r\n\t<br/>\r\n\t<a href=\"/employees\">List of employees</a>\r\n\t<br/>\r\n\t<br/>\r\n\t<a href=\"/users\">List of users</a>\r\n</body>\r\n</html>")));
 	}
 */
+//	test
 	@Test
 	@WithMockUser(username = "test_user", password = "test_pwd") // see test application.properties
 	public void testNotFoundPage() throws Exception {
